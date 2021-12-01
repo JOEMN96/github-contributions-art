4b38af12765a12dda59ecf4a45d3cd0592366862
JOE MON <56827756+JOEMN96@users.noreply.github.com>
2021-12-02 02:50:43 +0530

    Too many toops

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
-var svg = document.querySelector('svg');
-console.log(svg === null || svg === void 0 ? void 0 : svg.querySelectorAll('g g rect'));
+let svg = document.querySelector('svg');
+let rects = svg.querySelectorAll('g g');
+// data-level="0" --> no Highlight
+var o = [
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+];
+
+o.forEach((item) => {
+  item.forEach((val, index) => {
+    for (let i = 0; i < rects.length; i++) {}
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 let svg = document.querySelector('svg');
 
-console.log(svg?.querySelectorAll('g g rect'));
+let rects = svg?.querySelectorAll('g g ');
+console.log(rects[0]?.querySelectorAll('rect'));
+
+// data-level="0" --> no Highlight
+let o = [
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+];
